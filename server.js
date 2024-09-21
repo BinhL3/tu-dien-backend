54122ec267594498e8634382205422164d2e6716
bql23 <bql23@drexel.edu>
2024-09-20 23:57:48 -0700

    - added endpoint to get certain word's information

--- server.js
+++ server.js
@@ -20,6 +20,23 @@ app.post('/api/words', async (req, res) => {
   }
 })
 
+app.get('/api/words/title/:title', async (req, res) => {
+  const { title } = req.params;
+
+  try {
+    const word = await Word.findOne({ where: { title } });
+    if (!word) {
+      return res.status(404).json({ error: 'Word not found' });
+    }
+    res.json(word);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}); 
+
+// curl -X GET "http://localhost:8000/api/words/title/Ph%C3%B4ng%20b%E1%BA%A1t"
+
+
 app.get('/api/words', async (req, res) => {
   const limit = parseInt(req.query.limit, 10) || 10;
   const offset = parseInt(req.query.offset, 10) || 0; 
