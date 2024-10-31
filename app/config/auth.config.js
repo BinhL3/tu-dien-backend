e4b35926b8dd2f49b04256a0be934f558a20ab38
bql23 <bql23@drexel.edu>
2024-10-31 13:45:41 -0400

    checkduplicateusernamefunction

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "binhle-secret-key"
+}
--- server.js
+++ server.js
@@ -13,25 +13,9 @@ const Role = db.role;
 
 db.sequelize.sync({force: true}).then(() => {
   console.log("Drop and resync db");
-  initial();
+  // initial();
 })
 
-function initial() {
-  Role.create({
-    id: 1,
-    name: "user"
-  });
- 
-  Role.create({
-    id: 2,
-    name: "moderator"
-  });
- 
-  Role.create({
-    id: 3,
-    name: "admin"
-  });
-}
 
 Word.sync()
   .then(() => console.log('Database synced'))
